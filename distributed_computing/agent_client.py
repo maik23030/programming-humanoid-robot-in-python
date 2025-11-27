128d7dfc400a5b2202cbdae18dc50020cd4eec4f
maik2 <maik230300@gmail.com>
2025-11-27 18:38:22 +0100

    agent client

--- distributed_computing/agent_client.py
+++ distributed_computing/agent_client.py
@@ -95,5 +95,35 @@ class ClientAgent(object):
 if __name__ == '__main__':
     agent = ClientAgent()
     # TEST CODE HERE
+    # --- Blocking test: get/set angle ---
+    print("Testing blocking get_angle / set_angle...")
+
+    print("Setting LHipYawPitch to 0.3 rad...")
+    agent.set_angle("LHipYawPitch", 0.3)
+
+    val = agent.get_angle("LHipYawPitch")
+    print("Server returned angle =", val)
+
+    # --- Blocking test: transform retrieval ---
+    print("\nTesting get_transform...")
+    T = agent.get_transform("LLeg")
+    print("Transform of LLeg:", T)
+
+    # --- Non-blocking test: set_transform ---
+    print("\nTesting non-blocking call: post.set_transform...")
+    import time
+
+    thread = agent.post.set_transform("LLeg", [[1, 0, 0, 0],
+                                               [0, 1, 0, 0.05],
+                                               [0, 0, 1, -0.26],
+                                               [0, 0, 0, 1]])
+
+    print("Non-blocking call returned immediately:", thread)
+    print("Main thread continues running...")
+
+    # Wait a bit so background thread can finish
+    time.sleep(1.0)
+
+    print("Done.")
 
 
